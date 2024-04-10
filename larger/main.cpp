b83d436e0cb5122e6910c1ffebc04a1987b963ca
Isozaki Natsuki <72292434+Nattyu3@users.noreply.github.com>
2024-04-10 17:24:31 +0900

    Create main.cpp

---
+++ larger/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+  string n, m;
+  cin >> n >> m;
+
+  if (n[0] == '-' && m[0] != '-') {
+    cout << "No" << endl;
+    return 0;
+  }
+  if (n[0] != '-' && m[0] == '-') {
+    cout << "Yes" << endl;
+    return 0;
+  }
+  
+  int mi = (n[0] == '-');
+
+  string s = "", t = "";
+  string ss = "", tt = "";
+  for (int i = mi, ch = 0; i < (int)n.size(); i++) {
+    if (n[i] == '.') {
+      ch = 1;
+      continue;
+    }
+    if (!ch) s += n[i];
+    else ss += n[i];
+  }
+  for (int i = mi, ch = 0; i < (int)m.size(); i++) {
+    if (m[i] == '.') {
+      ch = 1;
+      continue;
+    }
+    if (!ch) t += m[i];
+    else tt += m[i];
+  }
+  reverse(all(s));
+  reverse(all(t));
+  int zs = s.size(), zt = t.size();
+  if (zs < zt) rep(i, zt-zs) s += '0';
+  if (zs > zt) rep(i, zs-zt) t += '0';
+  reverse(all(s));
+  reverse(all(t));
+  int zss = ss.size(), ztt = tt.size();
+  if (zss < ztt) rep(i, ztt-zss) ss += '0';
+  if (ztt > zss) rep(i, zss-ztt) tt += '0';
+
+  bool ch = true;
+  rep(i, s.size()) {
+    if (s[i] < t[i]) {
+      ch = false;
+      break;
+    }
+  }
+  rep(i, ss.size()) {
+    if (ss[i] < tt[i]) {
+      ch = false;
+      break;
+    }
+  }
+  
+  if ((ch ^ mi)) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
