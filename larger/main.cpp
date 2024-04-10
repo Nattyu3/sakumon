4924816a4e7f9c92bf77a1703d9ecc2896d616b8
Isozaki Natsuki <72292434+Nattyu3@users.noreply.github.com>
2024-04-10 18:14:46 +0900

    Update main.cpp

--- larger/main.cpp
+++ larger/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define all(x) (x).begin(), (x).end()
 
 void out(int x) {
   if (x) puts("Yes");
@@ -26,30 +25,29 @@ int main() {
   
   int mi = (n[0] == '-');
 
-  string s = "", t = "";
-  string ss = "", tt = "";
-  for (int i = mi, ch = 0; i < (int)n.size(); i++) {
+  string s = "", ss = "", t = "", tt = "";
+  for (int i = mi, c = 0; i < (int)n.size(); i++) {
     if (n[i] == '.') {
-      ch = 1;
+      c = 1;
       continue;
     }
-    if (!ch) s += n[i];
+    if (!c) s += n[i];
     else ss += n[i];
   }
-  for (int i = mi, ch = 0; i < (int)m.size(); i++) {
+  for (int i = mi, c = 0; i < (int)m.size(); i++) {
     if (m[i] == '.') {
-      ch = 1;
+      c = 1;
       continue;
     }
-    if (!ch) t += m[i];
+    if (!c) t += m[i];
     else tt += m[i];
   }
 
-  out(mi ^ (s.size() < t.size());
+  out(mi ^ (s.size() < t.size()));
 
   int zss = ss.size(), ztt = tt.size();
-  if (zss < ztt) rep(i, ztt-zss) ss += '0';
-  if (ztt > zss) rep(i, zss-ztt) tt += '0';
+  if (zss < ztt) for (int i = 0; i < ztt-zss; i++) ss += '0';
+  if (ztt > zss) for (int i = 0; i < zss-ztt; i++) tt += '0';
 
   bool ch = true;
   if (s < t) ch = false;
